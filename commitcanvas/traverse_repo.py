a6b95c9568e56e1c5e52542eae4b02da0fbf9685
bagashvilit <bagashvilit@allegheny.edu>
2020-06-26 13:47:15 +0400

    Refactor and add more data

--- commitcanvas/traverse_repo.py
+++ commitcanvas/traverse_repo.py
@@ -1,27 +1,38 @@
 """Code to traverse the Git History."""
 # pylint: disable=import-error
+import os
+
 import pandas as pd
 from pydriller import RepositoryMining
 
 
+def parse_file_type(name):
+    """Parse through file name and returns its format."""
+    if "." in name:
+        file_type, name = os.path.splitext(name)
+        file_type += ""
+        return name
+    return name
+
+
 def traverse(path):
     """Collect the data about the commit."""
-    message = []
-    file = []
-    change_type = []
+    data = {"message": [], "file_types": [], "change_types": [], "diff": []}
     for commit in RepositoryMining(path).traverse_commits():
 
+        file_type = []
         change_types = []
-        file_names = []
-        message.append(commit.msg)
-        file.append(file_names)
-        change_type.append(change_types)
+        diff_parsed = []
+
+        data["message"].append(commit.msg)
+        data["file_types"].append(file_type)
+        data["change_types"].append(change_types)
+        data["diff"].append(diff_parsed)
 
         for modif in commit.modifications:
             change_types.append(modif.change_type.name)
-            file_names.append(modif.filename)
-
-        data = {"message": message, "files": file, "change_types": change_type}
+            diff_parsed.append(modif.diff_parsed)
+            file_type.append(parse_file_type(modif.filename))
 
     return data
 
