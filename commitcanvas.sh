cc2bc55cff5d8fe3b100b08e9e2f305e645cd74a
bagashvilit <bagashvilit@allegheny.edu>
2020-07-17 14:59:14 +0400

    Modify output of commitcanvas

--- commitcanvas.sh
+++ commitcanvas.sh
@@ -1,10 +1,41 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 import shutil
 import sys
 from commitcanvas import commit_message_checker
+# # to make this script executable run following command: chmod +x commitcanvas.sh
+#
+# # save the commit message into the file, to make sure that it doesn't get lost
+# shutil.copy(sys.argv[1], '.git/pre-commit-saved-commit-msg')
+#
+# # get the commit message for the commit checks
+# with open('.git/pre-commit-saved-commit-msg', 'r') as file:
+#     data = file.read()
+#
+# PASSED = True
+#
+# list_of_checks = [
+#         commit_message_checker.check_length(data),
+#         commit_message_checker.check_for_period(data),
+#         commit_message_checker.check_capital_letter(data),
+#         commit_message_checker.check_blank_line(data),
+#         commit_message_checker.check_imperative_mood(data),
+# ]
+#
+# # get overall boolean value, to ensure that every item in the list has True value
+# # and therefore all the checks pass.
+# overall = all(list_of_checks)
+# # Display helpful tips on how to improve commit message
+# commit_message_checker.commit_check(data)
+#
+# if (PASSED != overall):
+#     exit (1)
+#
+# else:
+#     exit (0)
+
 # to make this script executable run following command: chmod +x commitcanvas.sh
 
-# save the commit message into the file, to make sure that it doesn't get lost
+# save the commit message into the file
 shutil.copy(sys.argv[1], '.git/pre-commit-saved-commit-msg')
 
 # get the commit message for the commit checks
@@ -13,22 +44,41 @@ with open('.git/pre-commit-saved-commit-msg', 'r') as file:
 
 PASSED = True
 
-list_of_checks = [
-        commit_message_checker.check_length(data),
-        commit_message_checker.check_for_period(data),
-        commit_message_checker.check_capital_letter(data),
-        commit_message_checker.check_blank_line(data),
-        commit_message_checker.check_imperative_mood(data),
-]
+length = commit_message_checker.check_length(data)
+period = commit_message_checker.check_for_period(data)
+capital = commit_message_checker.check_capital_letter(data)
+blank_line = commit_message_checker.check_blank_line(data)
+imperative = commit_message_checker.check_imperative_mood(data)
+
+list_of_checks = [length, period, capital, blank_line, imperative]
 
 # get overall boolean value, to ensure that every item in the list has True value
 # and therefore all the checks pass.
 overall = all(list_of_checks)
 # Display helpful tips on how to improve commit message
-commit_message_checker.commit_check(data)
 
 if (PASSED != overall):
+    if not capital:
+
+        print("Error: subject line should start with capital letter")
+
+    if not blank_line:
+
+        print("Error: blank line required between subject and a pharagraph")
+
+    if not length:
+
+        print("Error: keep less than 72 characters in the subject line")
+
+    if not period:
+
+        print("Error: no period needed at the end of the subject line")
+
+    if not imperative:
+
+        print("Error: start commit message with verb in imperative mood")
     exit (1)
 
 else:
+    # all the checks passed
     exit (0)
