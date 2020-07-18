88f6449e6761eb6bf4842037ea1feeb383468176
bagashvilit <bagashvilit@allegheny.edu>
2020-07-18 10:40:20 +0400

    Build the commitcanvas package and change the entry point for pre-commit

--- commitcanvas/__main__.py
+++ commitcanvas/__main__.py
@@ -1,17 +1,25 @@
 """Command line inferface."""
 # pylint: disable = import-error
-import typer
-
-
-APP = typer.Typer()
-
-
-@APP.command()
-def hello(message: str):
-    """Demo function."""
-    print(message)
+# import typer
 
 
 def main():
-    """Start execution of the program."""
-    APP()
+    """Demo function."""
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
+
+# APP = typer.Typer()
+#
+#
+# @APP.command()
+# def hello(message: str):
+#     """Demo function."""
+#     print(message)
+#
+#
+# def main():
+#     """Start execution of the program."""
+#     APP()
