485e9d5130cb494b027fabc73058ba3106e779a6
bagashvilit <bagashvilit@allegheny.edu>
2020-07-18 11:00:37 +0400

    Add checks to commitcanvas

--- commitcanvas/__main__.py
+++ commitcanvas/__main__.py
@@ -1,11 +1,12 @@
 """Command line inferface."""
 # pylint: disable = import-error
 # import typer
+from commitcanvas import commitcanvas_check
 
 
 def main():
-    """Demo function."""
-    print("Hello World")
+    """Demo."""
+    commitcanvas_check.commit_check()
 
 
 if __name__ == "__main__":
