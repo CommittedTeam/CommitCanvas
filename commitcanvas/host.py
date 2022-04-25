8d0f168f984d7cde0a5ddfa7eaddfdb3f65d2e14
bagashvilit <bagashvilit@allegheny.edu>
2022-04-24 23:01:27 -0400

    load plugins from user

--- commitcanvas/host.py
+++ commitcanvas/host.py
@@ -1,11 +1,9 @@
 import pluggy
-from commitcanvas import hookspecs,lib
+from commitcanvas import hookspecs
 import typer
 import sys
+import joblib
 
-pm = pluggy.PluginManager("commitcanvas")
-pm.add_hookspecs(hookspecs)
-pm.register(lib)
 
 
 app = typer.Typer()
@@ -20,9 +18,15 @@ def entry(path: str = None, commit: str = ".git/COMMIT_EDITMSG"):
     with open(commit_msg_filepath, "r+") as file:
         content = file.read()
         file.seek(0, 0)
-        print(content)
+
+
+        pm = pluggy.PluginManager("commitcanvas")
+        pm.add_hookspecs(hookspecs)
+        pm.register(path)
         pm.hook.checkm(message=content)
         pm.hook.checkl(message=content)
+
+        model = joblib.load(path)
 
         sys.exit(1)
 
