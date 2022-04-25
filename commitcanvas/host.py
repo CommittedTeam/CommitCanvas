d6c993cb8941d4463b5a53d330b1878bd38d79dc
bagashvilit <bagashvilit@allegheny.edu>
2022-04-25 16:45:48 -0400

    print list of errors

--- commitcanvas/host.py
+++ commitcanvas/host.py
@@ -32,7 +32,7 @@ def entry(path: str = None, commit: str = ".git/COMMIT_EDITMSG"):
             pm.register(obj[1]())
 
         errors = pm.hook.rule(message=content)
-
+        print(errors)
         if errors:
             print("\n")
             print(*errors, sep = "\n")
