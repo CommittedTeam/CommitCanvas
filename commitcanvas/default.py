5b95825c96338f69c5d6b778af811ce267d5f957
bagashvilit <bagashvilit@allegheny.edu>
2022-04-25 18:25:33 -0400

    add check for period at the end of line

--- commitcanvas/default.py
+++ commitcanvas/default.py
@@ -14,3 +14,11 @@ class subject_max_char_count:
         count = len(message)
         if count >= 100:
             return("Commit must have less than 100 characters, got: {}".format(count)) 
+
+
+class subject_endwith_period:
+    @commitcanvas.hookimpl
+    def rule(self,message:str):
+        splitted = message.splitlines()
+        if splitted[0].endswith("."):
+            return("Commit message subject can NOT end with period") 
