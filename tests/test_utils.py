27474cc0c910324c683dd1907c221ba589f3af5a
bagashvilit <bagashvilit@allegheny.edu>
2022-06-10 06:08:53 -0400

    test for exit code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import pytest
 from commitcanvas import utils
-
+from commitcanvas import hookspecs
+from commitcanvas import default
+import pluggy
+from inspect import getmembers, isclass
 
 def test_default_tokeep_single():
     """Check that single check is disabled correctly."""
@@ -23,5 +26,15 @@ def test_default_tokeep_no_space():
     remaining = utils.default_tokeep(disable)
 
     assert "subject_capital_letter" not in remaining
-    assert "subject_max_char_count" not in remaining
+    assert "subject_max_char_count" not in remaining    
 
+@pytest.mark.parametrize(
+    "input_errors,expected_value",
+    [(["error 1", "error 2"], 1), ([], 0)],
+)
+def test_exit_value(input_errors, expected_value):
+    """Determine if exit value is returned correctly."""
+    with pytest.raises(SystemExit) as exit_value:
+        utils.display_errors(input_errors)
+    assert exit_value.type == SystemExit
+    assert exit_value.value.code == expected_value
